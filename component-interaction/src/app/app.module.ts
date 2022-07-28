0b15baca78d55f20e3a37988d9e34075ee981615
Andrei-Suta <79984262+Andrei-Suta@users.noreply.github.com>
2022-07-28 15:54:22 +0300

    Revert "Revert "-Task1-""

--- component-interaction/src/app/app.module.ts
+++ component-interaction/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDetailsComponent } from './components/user-details.component/user-details.component';
+import { UserListComponent } from './components/user-list.component/user-list.component';
+import { IsAdultPipe } from './pipes/is-adult.pipe';
+import { UserAgePipe } from './pipes/user-age.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDetailsComponent,
+    UserListComponent,
+    IsAdultPipe,
+    UserAgePipe
   ],
   imports: [
     BrowserModule,
