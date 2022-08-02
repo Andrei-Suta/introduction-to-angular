5e047df42138dcd44184ef9faece56d503c6d21f
Daniel Ursu <57760647+danielursu@users.noreply.github.com>
2022-08-02 11:03:14 +0300

    Revert "Revert "-Task3-""

--- component-interaction/src/app/components/user-list.component/user-list.component.ts
+++ component-interaction/src/app/components/user-list.component/user-list.component.ts
@@ -4,40 +4,40 @@ import { UserModel } from "src/app/models/user.model";
 import { UserService } from "src/app/services/user.service";
 
 @Component({
-    selector: "user-list",
-    templateUrl: "./user-list.component.html",
-    styleUrls: ["./user-list.component.css"]
+	selector: "user-list",
+	templateUrl: "./user-list.component.html",
+	styleUrls: ["./user-list.component.css"]
 })
-export class UserListComponent implements OnInit{
-    public users?: UserModel[];
- 
+export class UserListComponent implements OnInit {
+	public users?: UserModel[];
 
-    public constructor(private userService: UserService){}
-    public ngOnInit(): void {
-        this.users = this.userService.getUsers();
-    }
+	public constructor(private userService: UserService) { }
 
-    public addUser(): void{
-        let email = prompt("Enter Your Email");
-        if (!email) { // deoarece prompt poate intoarce null, asignez empty
-            email = "";   // string in acest caz pentru a nu avea erori
-        }                // in cazul in care avem empty string, user-ul nu va fi creat si se va afisa o alerta
-        let name = prompt("Enter Your Name");
-        if (!name) {
-            name = "";
-        }
-        if (email.length === 0 || name.length === 0) {
-          alert("The New User Could Not Be Added");
-          return;
-        }
-        const newUser: UserModel = {
-                "email": email,
-                "dateOfBirth": new Date(),
-                "name": name
-         };
-         this.userService.addUser(newUser);
-        
-    }
-    
+	public ngOnInit(): void {
+		this.users = this.userService.getUsers();
+	}
+
+	public addUser(): void {
+		let email = prompt("Enter Your Email");
+		if (!email) { // deoarece prompt poate intoarce null, asignez empty
+			email = "";   // string in acest caz pentru a nu avea erori
+		}                // in cazul in care avem empty string, user-ul nu va fi creat si se va afisa o alerta
+		let name = prompt("Enter Your Name");
+		if (!name) {
+			name = "";
+		}
+		if (email.length === 0 || name.length === 0) {
+			alert("The New User Could Not Be Added");
+			return;
+		}
+		const newUser: UserModel = {
+			"email": email,
+			"dateOfBirth": new Date(),
+			"name": name
+		};
+		this.userService.addUser(newUser);
+
+	}
+
 
 }
--- component-interaction/src/app/pipes/user-age.pipe.ts
+++ component-interaction/src/app/pipes/user-age.pipe.ts
@@ -1,9 +1,12 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
-@Pipe({name: "userAge"})
-export class UserAgePipe implements PipeTransform{
-    public transform(date: Date): number{
-        let timeDiff = Math.abs(Date.now() - date.getTime());
-        return Math.floor((timeDiff / (1000 * 3600 * 24))/365.25);       
-    }
+@Pipe({ name: "userAge" })
+export class UserAgePipe implements PipeTransform {
+	public transform(date: Date | undefined): number {
+		if (date) {
+			let timeDiff = Math.abs(Date.now() - date.getTime());
+			return Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+		}
+		return 0;
+	}
 }
